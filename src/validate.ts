74ec709baf9c322ea54fec014a102822ba737d27
Tom Hudson <hudson_t@hotmail.co.uk>
2023-02-27 20:42:33 +0000

    chore: update validate function

--- src/validate.ts
+++ src/validate.ts
@@ -1,39 +1,85 @@
-import { rules } from './validation-rules'
-
-export interface ValidationRules {
-  type?: boolean
-  length?: boolean
-  prefix?: boolean
-  number?: boolean
-  suffix?: boolean
-}
+import { validNumbersObj, validPrefixesObj, validSuffixesObj } from './internal/data'
 
 interface ValidateResult {
-  rules: ValidationRules
+  reason?: string
   outcome: boolean
 }
 
 /**
  * Validates UK National Insurance numbers in accordance with https://www.gov.uk/hmrc-internal-manuals/national-insurance-manual/nim39110
  * @param nino
- * @param config
  * @returns ValidateResult
  */
 export const validate = (nino: string): ValidateResult => {
-  const result: ValidateResult = { outcome: true, rules: {} }
-
-  for (const key in rules) {
-    const outcome: boolean = rules[key](nino)
-    result.rules[key] = outcome
-
-    if (result.outcome) {
-      result.outcome = outcome
-    }
-
-    if (!outcome) {
-      break
+  if (typeof nino !== 'string') {
+    return {
+      reason: 'type invalid',
+      outcome: false
     }
   }
 
-  return result
+  if (nino.length !== 9) {
+    return {
+      reason: 'NINO must be 9 characters exactly',
+      outcome: false
+    }
+  }
+
+  if (validPrefixesObj[nino[0] + nino[1]] !== true) {
+    return {
+      reason: 'prefix invalid',
+      outcome: false
+    }
+  }
+
+  if (validNumbersObj[nino[2]] !== true) {
+    return {
+      reason: '1st digit invalid',
+      outcome: false
+    }
+  }
+
+  if (validNumbersObj[nino[3]] !== true) {
+    return {
+      reason: '2nd digit invalid',
+      outcome: false
+    }
+  }
+
+  if (validNumbersObj[nino[4]] !== true) {
+    return {
+      reason: '3th digit invalid',
+      outcome: false
+    }
+  }
+
+  if (validNumbersObj[nino[5]] !== true) {
+    return {
+      reason: '4th digit invalid',
+      outcome: false
+    }
+  }
+
+  if (validNumbersObj[nino[6]] !== true) {
+    return {
+      reason: '5th digit invalid',
+      outcome: false
+    }
+  }
+
+  if (validNumbersObj[nino[7]] !== true) {
+    return {
+      reason: '6th digit invalid',
+      outcome: false
+    }
+  }
+
+  if (validSuffixesObj[nino[8]] !== true) {
+    return {
+      reason: 'suffix invalid',
+      outcome: false
+    }
+  }
+
+  return { outcome: true }
 }
