e58a8c0c6306e2e080bd713e2e0ee9604545282b
Tom-Hudson <hudson_t@hotmail.co.uk>
2023-06-09 11:44:43 +0100

    chore: added benchmarks

--- benchmarks/validate/index.js
+++ benchmarks/validate/index.js
@@ -1,30 +1,48 @@
-const Benchmark = require('benchmark');
-const avris = require('avris-generator');
-const validNino = require("valid-nino").default;
-const isNiNumber = require('is-national-insurance-number');
-const { validate: testNinoValidate } = require('../../dist');
+const Benchmark = require('benchmark')
+const avris = require('avris-generator')
+const validNino = require('valid-nino').default
+const isNiNumber = require('is-national-insurance-number')
+const { validate: testNinoValidate } = require('../../dist')
 
-const validNinoStr = 'AA000000A';
+const scenarios = [
+  { scenario: 'valid', input: 'AA000000A' },
+  { scenario: 'invalid', input: 'A' },
+  { scenario: 'invalid', input: null },
+  { scenario: 'invalid', input: 'AAX00000A' },
+  { scenario: 'invalid', input: 'AA00000XA' }
+]
 
-const suite = new Benchmark.Suite;
+const execute = (input) => new Promise(resolve => {
+  const suite = new Benchmark.Suite()
 
-suite
+  suite
     .add('avris-generator', function () {
-        avris.validate('GB', 'nino', validNinoStr);
+      try {
+        avris.validate('GB', 'nino', input)
+      } catch (err) {
+        // Needed as avris will throw when invalid
+      }
     })
     .add('valid-nino', function () {
-        validNino(validNinoStr);
+      validNino(input)
     })
     .add('is-national-insurance-number', function () {
-        isNiNumber(validNinoStr);
+      isNiNumber(input)
     })
     .add('test-nino', function () {
-        testNinoValidate(validNinoStr);
-    })
-    .on('cycle', function (event) {
-        console.log(String(event.target));
+      testNinoValidate(input)
     })
     .on('complete', function () {
-        console.log('Fastest is ' + this.filter('fastest').map('name'));
+      const parsedResult = this.map(({ name, hz }) => ({ package: name, result: +hz.toPrecision(3) }))
+      resolve(parsedResult)
     })
-    .run();
+    .run()
+});
+
+(async () => {
+  for (const scenario of scenarios) {
+    console.log(`Scenario: ${scenario.scenario} (${scenario.input})`)
+    const result = await execute(scenario.input)
+    console.table(result)
+  }
+})()
