9e714b2f73a1c440634c9e9e5f5f1d383cc58798
Tom Hudson <hudson_t@hotmail.co.uk>
2025-02-11 00:10:55 +0000

    BREAKING CHANGE: drop deno support

--- deno_dist/normalise.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Normalises NINOs which have formatting in them
- * e.g. `AA 00 00 00 A` will be returned as `AA000000A`
- * @param nino
- * @returns string
- */
-export const normalise = (nino: string): string => nino.replace(/ |\t/g, '').toUpperCase()
